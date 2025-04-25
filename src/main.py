c3d0a13e7c209ed427bae41acf46e8aec94f817e
XiangqiUESTC <402214886@qq.com>
2025-04-26 03:13:01 +0800

    添加了异常捕获和异常的日志记录，删去了记录gpu数据的代码，添加了日志记录默认设备的代码

--- src/main.py
+++ src/main.py
@@ -11,73 +11,6 @@ import os
 from utils.logger import Logger
 
 
-# ----------------- GPU 监控线程 -----------------
-def gpu_monitor_thread_func(logfile, stop_flag, l):
-    """
-    ARGS:
-        logfile: gpu监测信息的输出文件的绝对路径
-        stop_flag: stop_flag是一个字典，字典属于非基本变量，通过字典里面的值来在外部控制进程的结束
-        l: 日志实例对象
-    DESCRIPTION:
-        gpu监控线程函数
-    """
-    # 新增目录创建逻辑
-    os.makedirs(dirname(logfile), exist_ok=True)
-    # 如果是昆仑芯NPU，则取消注释以下代码，并注释掉原有的监测GPU的代码
-    # l.info("MONITOR-NPU-THREAD")
-    # with open(logfile, 'w') as f:
-    #     kml_proc = subprocess.Popen(["kml-smi", "--query", "power", "--interval", "1000"], stdout=kml_log)
-    #     while not stop_flag["stop"]:
-    #         line = kml_proc.stdout.readline()
-    #         # l.info("LINE: ", line)
-    #         if line:
-    #             f.write(line)
-    #             f.flush()
-    #     proc.terminate()
-    # GPU监测代码
-    with open(logfile, 'w') as f:
-        f.write("timestamp  power.draw [W] util [%] memory [MiB]\n")
-        while not stop_flag["stop"]:
-            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            try:
-                output = subprocess.check_output([
-                    'nvidia-smi',
-                    '--query-gpu=power.draw,utilization.gpu,memory.used',
-                    '--format=csv,noheader,nounits'
-                ])
-                line = output.decode('utf-8').strip()
-                f.write(f"{timestamp},{line}\n")
-                f.flush()
-            except Exception as e:
-                l.error("GPU monitoring error:", e)
-                break
-
-
-# ----------------- CPU 监控线程（使用 turbostat） -----------------
-def cpu_monitor_thread_func(logfile, stop_flag, l):
-    """
-    ARGS:
-        logfile: cpu监测信息的输出文件的绝对路径
-        stop_flag: stop_flag是一个字典，字典属于非基本变量，通过字典里面的值来在外部控制进程的结束
-        l: 日志实例对象
-    DESCRIPTION:
-        cpu监控线程函数
-    """
-    # 新增目录创建逻辑
-    os.makedirs(dirname(logfile), exist_ok=True)
-    # 'turbostat' '--quiet' '--Summary' '--interval' '1'
-    cmd = ['turbostat', '--quiet', '--Summary', '--interval', '1']
-    with open(logfile, 'w') as f:
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, universal_newlines=True, bufsize=1)
-        while not stop_flag["stop"]:
-            line = proc.stdout.readline()
-            if line:
-                # l.info("LINE: ", line)
-                f.write(line)
-                f.flush()
-        proc.terminate()
-
-
 def write_csv(filename, data):
     """
     ARGS:
@@ -138,24 +71,18 @@ def operation_monitor(operation, operation_name, l, num_sample=1, loop_per_sampl
         # 生成此次测试的配置
         _ = operation.generate_config()
         # 装配数据
-        operation.setup()
+        try:
+            operation.setup()
+        except Exception as error:
+            l.error(error)
 
         # 预热GPU
         time.sleep(2)
         for _ in range(preheat):
-            operation.execute()
-
-        # 启动GPU监控线程
-        gpu_log = join(temp_dir, f'gpu_{operation_name}_{file_date}.csv')
-        stop_gpu = {"stop": False}
-        gpu_thread = threading.Thread(target=gpu_monitor_thread_func, args=(gpu_log, stop_gpu, l))
-        gpu_thread.start()
-
-        # 启动CPU监控线程
-        # cpu_log = join(temp_dir, f'cpu_{operation_name}_{file_date}.csv')
-        # stop_cpu = {"stop": False}
-        # cpu_thread = threading.Thread(target=cpu_monitor_thread_func, args=(cpu_log, stop_cpu, l))
-        # cpu_thread.start()
+            try:
+                operation.execute()
+            except Exception as error:
+                l.error(error)
 
         # 记录时间戳
         start_time = datetime.now().isoformat()
@@ -163,53 +90,18 @@ def operation_monitor(operation, operation_name, l, num_sample=1, loop_per_sampl
         start_time_ns = time.time_ns()
         # 重复执行，不断采样
         for _ in range(loop_per_sample):
-            operation.execute()
+            try:
+                operation.execute()
+            except Exception as error:
+                l.error(error)
         # 记录时间戳
         end_time_ns = time.time_ns()
         # 记录持续时间（毫秒）
         end_time = datetime.now().isoformat()
 
-        # Todo 此处执行完之后为什么不立刻停止线程？不怕采集到非算子运行时的数据吗？
         # 保证采样完整
         time.sleep(2)
-        # 停止监测线程
 
-        stop_gpu["stop"] = True
-        gpu_thread.join()
-        # stop_cpu["stop"] = True
-        # cpu_thread.join()
-
-        # 分析 GPU 数据
-        powers, utils, mems = [], [], []
-        with open(gpu_log, 'r') as f:
-            # 跳过文件头
-            next(f)
-            for data_line in f:
-                data_item = data_line.strip().split(',')
-                if len(data_item) == 4:
-                    powers.append(float(data_item[1]))
-                    utils.append(float(data_item[2]))
-                    mems.append(float(data_item[3]))
-        # 取平均,保留两位小数
-        avg_power = round(sum(powers) / len(powers), 2) if powers else 0
-        avg_util = round(sum(utils) / len(utils), 2) if utils else 0
-        avg_mem = round(sum(mems) / len(mems), 2) if mems else 0
-        # 有时候没有读到任何一条数据,避免max空列表出错,加入数据0
-        powers.append(0)
-        utils.append(0)
-        mems.append(0)
-        max_power = round(max(powers), 2)
-        max_util = round(max(utils), 2)
-        max_mem = round(max(mems), 2)
-
-        gpu_data = {
-            'avg_gpu_power': avg_power,
-            'arg_gpu_util': avg_util,
-            'arg_gpu_mem': avg_mem,
-            'max_gpu_power': max_power,
-            'max_gpu_util': max_util,
-            'max_gpu_mem': max_mem,
-        }
         # 计算时间
         duration = round((end_time_ns - start_time_ns)/loop_per_sample, 2)
 
@@ -221,26 +113,10 @@ def operation_monitor(operation, operation_name, l, num_sample=1, loop_per_sampl
 
         test_config = op.config
 
-        l.info(f"监测到数据\n{test_config}{gpu_data}{other_data}")
-
-        # 分析 CPU 数据（取 PKG(W) 字段平均）
-        # cpu_powers = []
-        # with open(cpu_log, 'r') as f:
-        #     for line in f:
-        #         # l.info("line: ", line)
-        #         if 'Avg_MHz' not in line:
-        #             parts = line.strip().split()
-        #             try:
-        #                 cpu_powers.append(float(parts[22]))
-        #             except:
-        #                 pass
-        # avg_cpu = round(sum(cpu_powers) / len(cpu_powers), 2) if cpu_powers else 0
-        # cpu_data = {
-        #     "avg_cpu": avg_cpu
-        # }
+        l.info(f"监测到数据\n{test_config}{other_data}")
 
         # 解析数据字典
-        dictionaries = [test_config, gpu_data, other_data]
+        dictionaries = [test_config, other_data]
         for dictionary in dictionaries:
             for key, value in dictionary.items():
                 # 第一次的时候需要初始化
@@ -257,12 +133,6 @@ def operation_monitor(operation, operation_name, l, num_sample=1, loop_per_sampl
 
 # ----------------- 主函数 -----------------
 if __name__ == '__main__':
-    # 参数设置
-    # op_names = [
-    #     "avg_pooling", "conv", "elu", "linear_layer", "max_pooling", "relu", "silu", "leaky_relu",
-    #     "spmm", "flatten", "cat", "lay_norm", "embedding", "positional_encoding", "roi_align",
-    #     "nms", "add", "softmax", "lstm"
-    # ]
     # 同时注册算子和模型
     REGISTRY = {
         **OPERATOR_REGISTRY,
@@ -306,6 +176,7 @@ if __name__ == '__main__':
         op = REGISTRY[op_name]()
 
         logger.info(f"对{op_name}的实验开始!测试{num_samples}次!")
+        logger.info(f"默认设备是{op.device}")
         operation_monitor(
             op,
             op_name,
