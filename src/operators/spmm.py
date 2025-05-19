910f68921cfd91cbacb78fae0c84a4de3ab5dba1
XiangqiUESTC <402214886@qq.com>
2025-05-19 17:35:14 +0800

    简化self.generator_config.xxx为self.xxx之后的spmm的对应修改

--- src/operators/spmm.py
+++ src/operators/spmm.py
@@ -16,18 +16,12 @@ class Spmm(BaseProcessor):
         self.A = None
 
         # 增加模式需要初始化一些变量
-        if self.generator_config.mode == "increase":
+        if self.mode == "increase":
             self.times = 0
-            self.m = self.generator_config.m
-            self.n = self.generator_config.n
-            self.p = self.generator_config.p
-            self.step_increment = self.generator_config.step_increment
-            self.sparsity = self.generator_config.start_sparsity
-            self.sparsity_increment = self.generator_config.sparsity_increment
 
     def generate_config(self):
         # 每个维度的区间
-        if self.generator_config.mode == "random":
+        if self.mode == "random":
             SINGLE_DIM_LENGTH_MAX = self.generator_config.single_dim_max_size
             SINGLE_DIM_LENGTH_MIN = self.generator_config.single_dim_min_size
             # 稀疏度
@@ -47,7 +41,7 @@ class Spmm(BaseProcessor):
             m = self.m + (self.times//3 + int(self.times % 3 >= 0)) * self.step_increment
             n = self.n + (self.times//3 + int(self.times % 3 >= 1)) * self.step_increment
             p = self.p + (self.times//3 + int(self.times % 3 >= 2)) * self.step_increment
-            sparsity = round(self.sparsity + self.sparsity_increment * self.times, 3)
+            sparsity = round(self.start_sparsity + self.sparsity_increment * self.times, 3)
 
             nnz = math.floor(m * p * sparsity)
 
