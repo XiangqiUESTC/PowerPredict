45f5ded68fe3365242f76c47c626936de0fe4f51
XiangqiUESTC <402214886@qq.com>
2025-06-17 20:17:42 +0800

    修改了部分cuda判断的逻辑

--- src/utils/device.py
+++ src/utils/device.py
@@ -4,7 +4,7 @@ import torch
 def is_device_avail_on_torch(device):
     if device == "cpu":
         return True
-    elif device == "cuda":
+    elif isinstance(device, str) and device.startswith("cuda"):
         return torch.cuda.is_available()
     elif device == "xpu":
         return hasattr(torch, 'xpu') and torch.xpu.is_available()
