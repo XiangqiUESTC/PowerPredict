a3823cc2295fac8cb4cd4499aa8128d268026c4e
XiangqiUESTC <402214886@qq.com>
2025-07-11 18:51:29 +0800

    预处理类接口的定义

--- src/train/core/base_preprocessor.py
+++ src/train/core/base_preprocessor.py
@@ -2,5 +2,12 @@ from abc import ABC, abstractmethod
 
 
 class BasePreprocessor(ABC):
-    def __init__(self):
+    def __init__(self, config, logger, raw_data):
+        self.config = config
+        self.logger = logger
+        self.raw_data = raw_data
+        pass
+
+    @abstractmethod
+    def process(self):
         pass
