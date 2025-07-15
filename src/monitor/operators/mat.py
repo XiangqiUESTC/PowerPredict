937353b8c7ef46a31aaee890f52d648d70dd5319
XiangqiUESTC <402214886@qq.com>
2025-07-16 02:34:31 +0800

    mat算子的修改

--- src/monitor/operators/mat.py
+++ src/monitor/operators/mat.py
@@ -10,6 +10,10 @@ class Mat(BaseProcessor):
         self.mat2 = None  # 相当于另一个矩阵
         self.output_tensor = None
 
+        # 增加模式需要初始化一些变量
+        if self.mode == "increase":
+            self.times = 0
+
     def generate_config(self):
         """
         生成矩阵乘法参数：
@@ -17,29 +21,37 @@ class Mat(BaseProcessor):
         - 权重形状: (K, N)
         - 输出形状: (M, N)
         """
-        # 随机生成矩阵维度（支持自定义默认参数）
-        input_dim = (random.randint(1, 1048), random.randint(1, 1048))
-        weight_dim = (input_dim[1], random.randint(1, 1048))
-        output_dim = (input_dim[0], weight_dim[1])
+        m = None
+        n = None
+        p = None
+
+        if self.mode == "increase":
+            # 随机生成矩阵维度（支持自定义默认参数）
+            m = self.m + (self.times//3 + int(self.times % 3 >= 0)) * self.step_increment
+            n = self.n + (self.times//3 + int(self.times % 3 >= 1)) * self.step_increment
+            p = self.p + (self.times//3 + int(self.times % 3 >= 2)) * self.step_increment
+
+            self.times += 1
 
         self.config = {
-            "mat1_shape": input_dim,
-            "mat2_shape": weight_dim,
-            "output_shape": output_dim
+            "m": m,
+            "p": n,
+            "n": p,
         }
 
     def setup(self):
         """生成输入张量"""
-        M, K = self.config["mat1_shape"]
-        K, N = self.config["mat2_shape"]
+        m = self.config["m"]
+        p = self.config["p"]
+        n = self.config["n"]
         self.mat1 = torch.randn(
-            M, K,
+            m, p,
             dtype=torch.float32,
             device=self.device
         )
         # 初始化矩阵2
         self.mat2 = torch.randn(
-            K, N,
+            p, n,
             dtype=torch.float32,
             device=self.device
         )
